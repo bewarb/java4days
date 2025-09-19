ab7611691899a241216877c691833e27b160c853
bewarb <pineapplepower04@gmail.com>
2025-09-19 07:12:09 -0400

    Add Magic8Ball Java project for Week 1

---
+++ Week-1/Magic8Ball/src/Magic8Ball.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] responses = {
+                "Girl, dw",
+                "You did that!",
+                "Is it a question?",
+                "YUPPPPP",
+                "Yes trusttttt",
+                "I'd be fooled",
+                "Like probably",
+                "Vibes good",
+                "Yes",
+                "Crystals say yes",
+                "lets think of something else",
+                "hell no.",
+                "chat says its cooked",
+                "time to move on",
+                "no shot",
+                "uhhhhh idk",
+                "dont bug me rn",
+                "use ur big head, and try again",
+                "shordy i cant rn",
+                "lowkey maybe"};
+
+    }
+}
