2678999a413eaa8e300cafac65183182cac23b6d
bewarb <pineapplepower04@gmail.com>
2025-09-21 13:00:29 -0400

    Some Magic8Ball Updates
    ASCII Art

--- Week-1/Magic8Ball/src/Magic8Ball.java
+++ Week-1/Magic8Ball/src/Magic8Ball.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Magic8Ball {
@@ -25,6 +26,38 @@ public class Magic8Ball {
                 "use ur big head, and try again",
                 "shordy i cant rn",
                 "lowkey maybe"};
+
+        Random rand = new Random();
+
+
+        System.out.println("        Welcome to Botobop's Magic 8ball!");
+        System.out.println("""
+                                        ____
+                                 dIi  (_)   G8888@b
+                                    ,dP9CGG88@b,
+                                  ,IP  _   Y888@@b,
+                                dCII  (_)   G8888@@b
+                                GCCIi     ,GG8888@@@
+                                GGCCCCCCCGGG88888@@@
+                                GGGGCCCGGGG88888@@@@...
+                                Y8GGGGGG8888888@@@@P.....
+                                 Y88888888888@@@@@P......
+                                 `Y8888888@@@@@@@P'......
+                                    `@@@@@@@@@P'.......
+                                        ""\""........
+                """);
+        System.out.println("I hope you have good questions! First, what is your name?");
+        String name = sc.nextLine();
+
+        System.out.println("Hi " + name +", do you have a Yes or No question for me?");
+
+        System.out.print("\nYour question (or 'quit' to exit): ");
+
+        if (sc.nextLine().equalsIgnoreCase("YES")) {
+
+        }
+
+
 
     }
 }
